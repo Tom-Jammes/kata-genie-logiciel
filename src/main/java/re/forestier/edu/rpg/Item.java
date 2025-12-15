e48f10f4bbd0a27fb180c59ec26ea36f8e300671
Tom Jammes <tom.jammes2@gmail.com>
2025-12-15 11:23:02 +0100

    Feature : Ajout des tous les items

--- src/main/java/re/forestier/edu/rpg/Item.java
+++ src/main/java/re/forestier/edu/rpg/Item.java
@@ -1,7 +1,61 @@
 package re.forestier.edu.rpg;
 
 public enum Item {
-    ;
+    LOOKOUT_RING(
+            "Lookout Ring",
+            "Prevents surprise attacks",
+            50,
+            0.1
+    ),
+
+    SCROLL_OF_STUPIDITY(
+            "Scroll of Stupidity",
+            "INT-2 when applied to an enemy",
+            30,
+            0.05
+    ),
+
+    DRAUPNIR(
+            "Draupnir",
+            "Increases XP gained by 100%",
+            200,
+            0.3
+    ),
+
+    MAGIC_CHARM(
+            "Magic Charm",
+            "Magic +10 for 5 rounds",
+            75,
+            0.1
+    ),
+
+    RUNE_STAFF_OF_CURSE(
+            "Rune Staff of Curse",
+            "May burn your enemies... Or yourself. Who knows?",
+            150,
+            2.5
+    ),
+
+    COMBAT_EDGE(
+            "Combat Edge",
+            "Well, that's an edge",
+            40,
+            1.0
+    ),
+
+    HOLY_ELIXIR(
+            "Holy Elixir",
+            "Recover your HP",
+            100,
+            0.5
+    ),
+
+    MAGIC_BOW(
+            "Magic Bow",
+            "A mystical bow with arcane power",
+            500,
+            2.0
+    );
 
     private final String name;
     private final String description;
